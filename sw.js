f6d34e74bf56866e39333043b9090869bbfbd949
CTM5150 <instigator9@gmail.com>
2026-08-09 22:44:50 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-const CACHE="rewardloop-v1";
-const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C="rewardloop-polished";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(x=>x.addAll(["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
